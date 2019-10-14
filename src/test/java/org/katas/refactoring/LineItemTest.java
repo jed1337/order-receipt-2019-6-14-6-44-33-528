f331943784b2eaa440a6871108418baf99f00868
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-14 18:11:09 +0800

    Add tests for line item

--- src/test/java/org/katas/refactoring/LineItemTest.java
+++ src/test/java/org/katas/refactoring/LineItemTest.java
@@ -1,10 +1,8 @@
 package org.katas.refactoring;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class LineItemTest {
     @Test
