fb1b432e925ca9bb8988789ba7c1016d24191e1b
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-14 18:11:00 +0800

    Add

---
+++ src/test/java/org/katas/refactoring/LineItemTest.java
@@ -0,0 +1,30 @@
+package org.katas.refactoring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LineItemTest {
+    @Test
+    public void shouldGetTotalAmount() {
+        LineItem peanuts = new LineItem("peanuts", 45.0, 2);
+
+        assertThat(peanuts.totalAmount()).isEqualTo(90.0);
+    }
+
+    @Test
+    public void shouldGetTotalAmountOfFreeItem() {
+        LineItem peanuts = new LineItem("sticker", 0, 20);
+
+        assertThat(peanuts.totalAmount()).isEqualTo(0.0);
+    }
+
+    @Test
+    public void shouldGetTotalAmountOfPriceWithDecimalPoint() {
+        LineItem peanuts = new LineItem("pie", 3.14, 314);
+
+        assertThat(peanuts.totalAmount()).isEqualTo(985.96);
+    }
+}
